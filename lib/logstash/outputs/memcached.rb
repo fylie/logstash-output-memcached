f282f5fbfda104570350e306cd3fe175d88ddf25
fylie <kylieafisher@gmail.com>
2016-11-01 02:06:33 -0400

    It 'works'

--- lib/logstash/outputs/memcached.rb
+++ lib/logstash/outputs/memcached.rb
@@ -1,17 +1,54 @@
 # encoding: utf-8
-require "logstash/outputs/base"
+require "logstash/filters/base"
 require "logstash/namespace"
 
-# An memcached output that does nothing.
+
 class LogStash::Outputs::Memcached < LogStash::Outputs::Base
+
+  # memcached {
+  #    {
+  #     host => "localhost"
+  #     port => 11211
+  #     key => "memcache_key"
+  #     value => "%{my_field}"
+  #   }
+  # }
+  #
   config_name "memcached"
+
+  # Memcached host
+  config :host, :validate => :string, :default => "localhost", :required => true
+
+  # Memcached port
+  config :port, :validate => :number, :default => 11211, :required => true
+
+  # Memcached key
+  config :key, :validate => :string, :required => true
+
+  # The value to be stored. If there is no value, this defaults to the event as
+  # JSON.
+  config :value, :validate => :string, :default => ""
+
+  # TTL (how long to store the key in seconds)
+  config :ttl, :validate => :number, :default => 0
+
 
   public
   def register
+    require 'dalli'
+    require 'json'
+    options = {:expires_in => @ttl}
+    @memcached = Dalli::Client.new("#{@host}:#{@port}", options)
   end # def register
 
   public
   def receive(event)
-    return "Event received"
-  end # def event
+    key = event.sprintf(@key)
+    if @value.empty?
+      @memcached.set(key, event.to_json)
+    else
+      value = event.sprintf(@value)
+      @memcached.set(key, value)
+    end
+  end
 end # class LogStash::Outputs::Memcached
--- logstash-output-memcached.gemspec
+++ logstash-output-memcached.gemspec
@@ -2,9 +2,8 @@ Gem::Specification.new do |s|
   s.name          = 'logstash-output-memcached'
   s.version       = '0.1.0'
   s.licenses      = ['Apache License (2.0)']
-  s.summary       = 'TODO: Write a short summary, because Rubygems requires one.'
-  s.description   = 'TODO: Write a longer description or delete this line.'
-  s.homepage      = 'TODO: Put your plugin''s website or public repo URL here.'
+  s.summary       = 'Sets items in memcached.'
+  s.homepage      = 'https://github.com/fylie/logstsh-output-memcached.git'
   s.authors       = ['fylie']
   s.email         = 'kylieafisher@gmail.com'
   s.require_paths = ['lib']
@@ -20,5 +19,7 @@ Gem::Specification.new do |s|
   # Gem dependencies
   s.add_runtime_dependency "logstash-core-plugin-api", ">= 1.60", "<= 2.99"
   s.add_runtime_dependency "logstash-codec-plain"
+  s.add_runtime_dependency "dalli"
+  s.add_runtime_dependency "json"
   s.add_development_dependency "logstash-devutils"
 end
